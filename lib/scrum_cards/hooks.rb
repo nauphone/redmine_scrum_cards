7c58f0a3006ae7abcfc53a48faf4bf77c8ae3bb7
Evgeny Tataurov <etataurov@naumen.ru>
2014-03-13 11:15:00 +0600

    usage redmine_extend_hooks plugin

--- lib/scrum_cards/hooks.rb
+++ lib/scrum_cards/hooks.rb
@@ -1,6 +1,6 @@
 module RedmineScrumCards
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_issues_context_menu_end,
-	      :partial => 'hooks/scrum_cards/view_issues_context_menu_end'
+    render_on :view_issues_context_menu_end, :partial => 'hooks/scrum_cards/view_issues_context_menu_end'
+    render_on :view_issues_show_action_menu, :partial => 'hooks/scrum_cards/action_menu_hook'
   end
 end
